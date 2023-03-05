a22b196c336f4de4d6447f465a043381933713fe
Dave O'Connor <doc@skynet.ie>
2023-03-05 14:58:53 -0800

    Update launch to be multi-monitor

--- simple/forest/launch.sh
+++ simple/forest/launch.sh
@@ -11,4 +11,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+done
